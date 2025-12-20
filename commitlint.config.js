970e204b93866cdab2efbab6c83aed26bd325bbf
FOV-RGT <Q1950088436@outlook.com>
2025-12-20 22:52:08 +0800

    chore: commitlint config - add deploy and snapshot types

--- commitlint.config.js
+++ commitlint.config.js
@@ -17,6 +17,8 @@ export default {
                 'ci',
                 'build',
                 'release',
+                '[deploy]',
+                '[snapshot]',
             ],
         ],
     },
